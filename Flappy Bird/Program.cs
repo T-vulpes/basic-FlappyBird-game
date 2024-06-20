2f4c55e79f1326b32aa51fcb92f682e7403340c3
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-06-20 15:28:11 +0300

    Program.cs

--- Flappy Bird/Program.cs
+++ Flappy Bird/Program.cs
@@ -8,9 +8,6 @@ namespace Flappy_Bird
 {
     static class Program
     {
-        /// <summary>
-        /// Uygulamanın ana girdi noktası.
-        /// </summary>
         [STAThread]
         static void Main()
         {
